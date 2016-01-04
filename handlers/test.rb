8d9c80644009b9a9062222beee8cc64a441ca947
Jon <jonnmb@Gmail.com>
2016-01-03 22:24:42 -0700

    Clearing test data

--- handlers/test.rb
+++ handlers/test.rb
@@ -1,15 +1,7 @@
 class Test
 
 	def self.hole
-		testGrow = Grow.new
-		testPlant = Plant.new
-		testPlant.strain = "brad Kush"
-		testGrow.plants << testPlant
-		testPlant2 = Plant.new
-		testPlant2.strain = "BRAD SUCKS"
-		testGrow.plants << testPlant2
-		puts "test grow plants"
-		puts testGrow.plants.to_json
+		puts "hello world"
 	end
 
 end
